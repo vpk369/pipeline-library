a65f5eae05f2a5a85a3d26fd3ce2e72d64d1a799
Pradeep Vijayagiri <vpk369@gmail.com>
2019-06-21 19:23:09 +0530

    formatting fix

--- vars/NotifyMaxBuildSizeViaEmail.groovy
+++ vars/NotifyMaxBuildSizeViaEmail.groovy
@@ -14,7 +14,7 @@ def call(Map pipelineParams) {
         echo "Build size exceeded max size of " + pipelineParams.MAX_BUILD_SIZE_IN_MB + " MB set for this job"
         wrap([$class: 'BuildUser']) { //requires jenkins plugin ‘user build vars’ to be enabled
             emailext( //requires jenkins plugin ‘extension e-mail’ to be enabled
-                body: "Hi ${BUILD_USER_FIRST_NAME},\n\nYour Jenkins job ${env.JOB_NAME} with build ${env.BUILD_NUMBER}\nhas exceeded max build size of " + pipelineParams.MAX_BUILD_SIZE_IN_MB + " MB set in \n${env.JOB_URL}.\n\nThank you,\nTeam CSE",
+                body: "Hi ${BUILD_USER_FIRST_NAME},\n\nYour Jenkins job ${env.JOB_NAME} \nwith build ${env.BUILD_NUMBER} has exceeded max build size of " + pipelineParams.MAX_BUILD_SIZE_IN_MB + " MB set in \n${env.JOB_URL}.\n\nThank you,\nTeam CSE",
                 recipientProviders: [[$class: 'RequesterRecipientProvider']],
                 subject: "Jenkins Build size limit exceeded for Job ${env.JOB_NAME}"
             ) 
